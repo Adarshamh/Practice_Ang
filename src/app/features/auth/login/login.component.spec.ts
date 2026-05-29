7d7941cbf00a42dfac6012dd3ac4fb323d6558af
Adarshmh <Adarshmh>
2026-05-29 09:41:00 +0530

    http client in testing files

--- src/app/features/auth/login/login.component.spec.ts
+++ src/app/features/auth/login/login.component.spec.ts
@@ -1,37 +1,28 @@
+/// <reference types="jasmine" />
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
-
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('LoginComponent', () => {
-
   let component: LoginComponent;
-
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
-
     await TestBed.configureTestingModule({
-
       imports: [LoginComponent],
-
       providers: [
-        provideRouter([])
+        provideRouter([]),
+        provideHttpClient()
       ]
-
     }).compileComponents();
 
-    fixture =
-      TestBed.createComponent(LoginComponent);
-
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-
     fixture.detectChanges();
   });
 
   it('should create', () => {
-
     expect(component).toBeTruthy();
   });
 });
