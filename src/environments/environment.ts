fe05856a4b1c208e8dd5b1891464d71c45bafdd7
Adarshmh <Adarshmh>
2026-06-04 20:00:30 +0530

    new ip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,6 @@ export const environment = {
   //apiUrl: 'http://localhost:8080/api'
 
   // Google Cloud VM Run
-  apiUrl: 'http://34.14.181.243:8080/api'
+  //apiUrl: 'http://34.14.181.243:8080/api'
+  apiUrl: 'http://34.93.105.99:8080/api'
 };
