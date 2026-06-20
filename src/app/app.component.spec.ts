8b13ee61a9912714349d535f8bba6e0120cfdddb
Adarshmh <Adarshmh>
2026-06-20 22:26:17 +0530

    test failure httpclient not provided

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AppComponent } from './app.component';
 
@@ -7,9 +9,11 @@ describe('AppComponent', () => {
   beforeEach(async () => {
 
     await TestBed.configureTestingModule({
-
-      imports: [AppComponent],
-
+      imports: [
+        AppComponent,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
     }).compileComponents();
   });
 
